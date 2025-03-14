8dbe51a692c6d1d36e2f75eca81b62151fab7fcd
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-03-14 17:13:01 -0600

    Issue #3
    Run example code.

--- scrape_training_data.py
+++ scrape_training_data.py
@@ -1,91 +1,23 @@
 import os
-import json
-import requests
-import time
 from dotenv import load_dotenv
-
-
-def fetch_all_results(base_url, params, delay=5):
-    """
-    Fetches paginated data from MarketStack API
-    and returns a concatenated list of all results.
-
-    Parameters
-    ----------
-    base_url : str
-        The base URL of the request
-
-    params : dict
-        Dictionary of parameters for the request
-
-    delay : int, optional
-        Defaults to 5. Delay between API calls in seconds.
-
-    Returns
-    -------
-    list
-        List with all results concatenated.
-    """
-    all_results = []
-    offset = 0
-    limit = params["limit"]
-
-    while True:
-        params["offset"] = offset
-        print(f"Fetching offset {offset}")
-        time.sleep(delay)
-        response = requests.get(base_url, params=params)
-
-        if response.status_code != 200:
-            print(f"Error: {response.status_code}, {response.text}")
-            break
-
-        data = response.json()
-        results = data.get("data", [])
-        pagination = data.get("pagination", {})
-        print(pagination)
-
-        all_results.extend(results)
-
-        total = pagination.get("total", 0)
-
-        # If we've fetched all available records, stop
-        if offset + limit >= total:
-            break
-
-        # Move to the next offset
-        offset += limit
-
-        # Delay for the next API call
-        time.sleep(delay)
-
-    return all_results
+from polygon import RESTClient
 
 
 if __name__ == "__main__":
     # Load the .env file
     load_dotenv()
 
-    date_from = "2015-04-01"
-    date_to = "2025-03-12"
-
     # Paremters common to all API calls
-    marketstack_api_key = os.getenv("MARKETSTACK_API_KEY")
-    base_url = "http://api.marketstack.com/v2/eod"
+    polygon_io_api_key = os.getenv("POLYGON_IO_API_KEY")
 
-    # Scrape the symbols
-    symbols = ["AAPL", "GOOG", "AMZN", "MSFT", "NVDA", "TSLA"]
-    for symbol in symbols:
-        params = {
-            "access_key": marketstack_api_key,
-            "symbols": symbol,
-            "date_from": date_from,
-            "date_to": date_to,
-            "limit": 3000,
-            "sort": "ASC",
-        }
-        all_data = fetch_all_results(base_url, params)
-        print(f"Fetched {len(all_data)} records")
-        history_filename = os.path.join("output", f"{symbol}_history.json")
-        with open(history_filename, "w") as file:
-            json.dump(all_data, file, indent=4, sort_keys=True)
+    # Run example code
+    client = RESTClient(polygon_io_api_key)
+
+    # List Aggregates (Bars)
+    ticker = "AMZN"
+    aggs = []
+    for a in client.list_aggs(ticker=ticker, multiplier=1, timespan="day", from_="2022-06-01", to="2022-06-10", limit=100):
+        aggs.append(a)
+
+    for agg in aggs:
+        print(agg)
