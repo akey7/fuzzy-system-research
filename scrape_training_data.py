4d1fb6536dfbf0bb1b12bf77951dcef007cb4e83
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-03-14 18:54:43 -0600

    Issue #3
    Get 5 years of ticker histories.

--- scrape_training_data.py
+++ scrape_training_data.py
@@ -1,9 +1,10 @@
 import os
 from dotenv import load_dotenv
 from polygon import RESTClient
+import pandas as pd
 
 
-if __name__ == "__main__":
+def main():
     # Load the .env file
     load_dotenv()
 
@@ -13,11 +14,39 @@ if __name__ == "__main__":
     # Run example code
     client = RESTClient(polygon_io_api_key)
 
-    # List Aggregates (Bars)
-    ticker = "AMZN"
-    aggs = []
-    for a in client.list_aggs(ticker=ticker, multiplier=1, timespan="day", from_="2022-06-01", to="2022-06-10", limit=100):
-        aggs.append(a)
+    # Date range
+    date_from = "2020-04-01"
+    date_to = "2025-03-13"
 
-    for agg in aggs:
-        print(agg)
+    # List Aggregates (Bars)
+    tickers = ["AMZN", "GOOG", "MSFT", "AAPL", "NVDA", "TSLA"]
+    rows = []
+    for ticker in tickers:
+        for a in client.list_aggs(
+            ticker=ticker,
+            multiplier=1,
+            timespan="day",
+            from_="2022-06-01",
+            to="2022-06-10",
+            adjusted="true",
+        ):
+            rows.append({
+                "timestamp": a.timestamp,
+                "ticker": ticker,
+                "open": a.open,
+                "high": a.high,
+                "low": a.low,
+                "close": a.close,
+                "volume": a.volume,
+                "vwap": a.vwap,
+                "transactions": a.transactions,
+            })
+
+    # Return DataFrame
+    ticker_histories = pd.DataFrame(rows)
+    ticker_histories_filename = os.path.join("input", "ticker_histories.csv")
+    ticker_histories.to_csv(ticker_histories_filename, index=False)
+
+
+if __name__ == "__main__":
+    main()
