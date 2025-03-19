2a2b9ad48c5b78185fdc18e4ce6ffecceaf71aaf
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-03-19 14:23:46 -0600

    Issue #11
    Add docstrings to refactored functions.

--- arima_models.py
+++ arima_models.py
@@ -13,7 +13,83 @@ from sklearn.metrics import mean_squared_error
 import seaborn as sns
 
 
+def train_arima(ts, max_p=5, d=0, max_q=5, train_len=120, n_workers=6):
+    """
+    Initiate training runs of ARIMA models with ps and qs under the
+    given maximums to find the best pa and q values for the modeling
+    the given time series.
+
+    Parameters
+    ----------
+    ts : pd.Series
+        The time series to be modeled
+    
+    max_p : int, optional
+        The maximum number of lagged values to test. Defaults to 5.
+
+    max_q : int, optional
+        The maximum number of lagged disturbances to test. Defaults to 5.
+
+    train_len : int, optional
+        Then length of the training set. Defaults to 120.
+
+    n_workers : int, optional
+        Number of cores to use for parallel evaluation of models.
+        Defaults to 6.
+
+    Returns
+    -------
+    pd.DataFrame
+        Returns a DataFrame, meant for analysis by other functions in this
+        module, that is the results of training with different p and q values.
+    """
+    tasks = []
+    for p in range(max_p):
+        for q in range(max_q):
+            if p == 0 and q == 0:
+                continue
+            tasks.append(
+                {
+                    "ts": ts,
+                    "d": d,
+                    "train_len": train_len,
+                    "p": p,
+                    "q": q,
+                }
+            )
+    dask_client = Client(n_workers=n_workers)
+    futures = [dask_client.submit(p_d_q_fit, task) for task in tasks]
+    arima_fit_results = []
+    for future in tqdm(
+        as_completed(futures), total=len(futures), desc="Processing", unit="task"
+    ):
+        arima_fit_results.append(future.result())
+    dask_client.close()
+    arima_fit_df = pd.DataFrame(arima_fit_results)
+    arima_fit_df.set_index(["p", "q"], inplace=True)
+    return arima_fit_df
+
+
 def p_d_q_fit(task):
+    """
+    Train and evaluate an ARIMA model with the given p, d, and q parameters
+    in the task dictionary.
+    
+    Meant to be used by the train_arima function to perform parrallizable
+    tasks on cores.
+
+    Paremters
+    ---------
+    task : dict
+        Dictionary that defines the model training task from train_arima.
+
+    Returns
+    -------
+    dict
+        Returns a dictionary, suitable to be used as one row of the results
+        DataFrame returned by train_arima, that contains the results of the
+        training.
+    """
     ts = task["ts"]
     p = task["p"]
     d = task["d"]
@@ -57,35 +133,17 @@ def p_d_q_fit(task):
     return result
 
 
-def train_arima(ts, max_p=5, d=1, max_q=5, train_len=120, n_workers=6):
-    tasks = []
-    for p in range(max_p):
-        for q in range(max_q):
-            if p == 0 and q == 0:
-                continue
-            tasks.append(
-                {
-                    "ts": ts,
-                    "d": d,
-                    "train_len": train_len,
-                    "p": p,
-                    "q": q,
-                }
-            )
-    dask_client = Client(n_workers=n_workers)
-    futures = [dask_client.submit(p_d_q_fit, task) for task in tasks]
-    arima_fit_results = []
-    for future in tqdm(
-        as_completed(futures), total=len(futures), desc="Processing", unit="task"
-    ):
-        arima_fit_results.append(future.result())
-    dask_client.close()
-    arima_fit_df = pd.DataFrame(arima_fit_results)
-    arima_fit_df.set_index(["p", "q"], inplace=True)
-    return arima_fit_df
-
-
 def train_arima_viz(train_result):
+    """
+    In a Jupyter notebook, display heatmaps of p and q parameters and their
+    RMSE and BIC values to visualize which models did best.
+
+    Parameters
+    ----------
+    train_result : pd.DataFrame
+        The results of training a bunch of ARIMA models as returned by
+        train_arima.
+    """
     fig, axes = plt.subplots(ncols=2, figsize=(10, 4), sharex=True, sharey=True)
     sns.heatmap(
         train_result[train_result.rmse < 0.5].rmse.unstack().mul(10),
@@ -109,6 +167,11 @@ def train_arima_viz(train_result):
 
 
 def best_arima_model(ts, train_result):
+    """
+    Use training results from train_arima and make a final ARIMA
+    model that has the best p and q values, as determined by their
+    RMSE and BIC metrics.
+    """
     best_p, best_q = train_result.rank().loc[:, ["rmse", "mean_bic"]].mean(1).idxmin()
     print(best_p, best_q)
     best_arima_model = tsa.ARIMA(endog=ts, order=(best_p, 0, best_q)).fit()
@@ -116,6 +179,28 @@ def best_arima_model(ts, train_result):
 
 
 def plot_correlogram(ts0, nlags, title, residual_rolling=21, acf_plot_ymax=0.1):
+    """
+    In a Jupyter notebook, display the correlogram with ACF, PACF, and residuals QQ
+    plot and time series. Also displays Q and ADF stats and the moments of the 
+    residual distribution. This can assist with determining proper p and q ranges to
+    try and/or looking at the quality of the best fit model.
+
+    Parameters
+    ----------
+    ts0
+        Time series to be plotted.
+
+    nlags : int
+        Number of lags in the ACF and PACF diagrams.
+
+    residual_rolling : int, optional
+        The window for the rolling average in the residual plot.
+        Defaults to 21
+
+    acf_plot_ymax : float, optional
+        The y limits on the ACF and PACF plots will match the
+        magnitude of this value. Should be > 0.0. Defaults to 0.1
+    """
     ts = ts0.dropna()
     q_p = np.max(q_stat(acf(ts, nlags=nlags), len(ts))[1])
     stats = f"Q-Stat: {np.max(q_p):>8.2f}\nADF: {adfuller(ts)[1]:>11.2f}"
