325acc1bb1a33df79a4f64f4beac19c4763b7a5e
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-03-20 08:47:46 -0600

    Issue #17
    Move functions in the class.

--- arima_models.py
+++ arima_models.py
@@ -13,144 +13,185 @@ from sklearn.metrics import mean_squared_error
 import seaborn as sns
 
 
-def log_diff_prep(original_ts):
-    """
-    Prepare a time series for modeling by log transforming and
-    differencing. The date time index is passed through
-    unaffected.
+class ArimaModels:
+    def __init__(self, n_workers=6):
+        self.train_result = None
+        self.n_workers = n_workers
 
-    Parameters
-    ----------
-    original_ts : pd.Series
-        The original time series to be transformed.
+    def fit(original_ts, max_p=5, d=0, max_q=5, train_len=120):
+        pass
 
-    Returns
-    -------
-    pd.Series
-        The transformed time series.
-    """
-    return np.log(original_ts).diff()
+    def log_diff_prep(self, original_ts):
+        """
+        Prepare a time series for modeling by log transforming and
+        differencing. The date time index is passed through
+        unaffected.
 
+        Parameters
+        ----------
+        original_ts : pd.Series
+            The original time series to be transformed.
 
-def train_arima(ts, max_p=5, d=0, max_q=5, train_len=120, n_workers=6):
-    """
-    Initiate training runs of ARIMA models with ps and qs under the
-    given maximums to find the best pa and q values for the modeling
-    the given time series.
-
-    Parameters
-    ----------
-    ts : pd.Series
-        The time series to be modeled. The raw data should be transformed
-        by log_diff_prep() first.
-
-    max_p : int, optional
-        The maximum number of lagged values to test. Defaults to 5.
-
-    max_q : int, optional
-        The maximum number of lagged disturbances to test. Defaults to 5.
-
-    train_len : int, optional
-        Then length of the training set. Defaults to 120.
-
-    n_workers : int, optional
-        Number of cores to use for parallel evaluation of models.
-        Defaults to 6.
-
-    Returns
-    -------
-    pd.DataFrame
-        Returns a DataFrame, meant for analysis by other functions in this
-        module, that is the results of training with different p and q values.
-    """
-    tasks = []
-    for p in range(max_p):
-        for q in range(max_q):
-            if p == 0 and q == 0:
-                continue
-            tasks.append(
-                {
-                    "ts": ts,
-                    "d": d,
-                    "train_len": train_len,
-                    "p": p,
-                    "q": q,
-                }
-            )
-    dask_client = Client(n_workers=n_workers)
-    futures = [dask_client.submit(p_d_q_fit, task) for task in tasks]
-    arima_fit_results = []
-    for future in tqdm(
-        as_completed(futures), total=len(futures), desc="Processing", unit="task"
-    ):
-        arima_fit_results.append(future.result())
-    dask_client.close()
-    arima_fit_df = pd.DataFrame(arima_fit_results)
-    arima_fit_df.set_index(["p", "q"], inplace=True)
-    return arima_fit_df
+        Returns
+        -------
+        pd.Series
+            The transformed time series.
+        """
+        return np.log(original_ts).diff()
 
 
-def p_d_q_fit(task):
-    """
-    Train and evaluate an ARIMA model with the given p, d, and q parameters
-    in the task dictionary.
+    def train_arima(self, ts, max_p=5, d=0, max_q=5, train_len=120, n_workers=6):
+        """
+        Initiate training runs of ARIMA models with ps and qs under the
+        given maximums to find the best pa and q values for the modeling
+        the given time series.
 
-    Meant to be used by the train_arima function to perform parrallizable
-    tasks on cores.
+        Parameters
+        ----------
+        ts : pd.Series
+            The time series to be modeled. The raw data should be transformed
+            by log_diff_prep() first.
 
-    Paremters
-    ---------
-    task : dict
-        Dictionary that defines the model training task from train_arima.
+        max_p : int, optional
+            The maximum number of lagged values to test. Defaults to 5.
 
-    Returns
-    -------
-    dict
-        Returns a dictionary, suitable to be used as one row of the results
-        DataFrame returned by train_arima, that contains the results of the
-        training.
-    """
-    ts = task["ts"]
-    p = task["p"]
-    d = task["d"]
-    q = task["q"]
-    train_len = task["train_len"]
-    y_true = ts[train_len:]
-    y_pred = []
-    bics = []
-    aics = []
-    n_convergence_errors = 0
-    n_stationarity_errors = 0
-    for bound in range(train_len, len(ts)):
-        train_set = ts.iloc[bound - train_len : bound]
-        with warnings.catch_warnings():
-            warnings.filterwarnings("ignore")
-            try:
-                model = tsa.ARIMA(endog=train_set, order=(p, d, q)).fit()
-            except LinAlgError:
-                n_convergence_errors += 1
-            except ValueError:
-                n_stationarity_errors += 1
-            forecast = model.forecast(steps=1)
-            y_pred.append(forecast.iloc[0])
-            aics.append(model.aic)
-            bics.append(model.bic)
-    df = (
-        pd.DataFrame({"y_true": y_true, "y_pred": y_pred})
-        .replace(np.inf, np.nan)
-        .dropna()
-    )
-    rmse = np.sqrt(mean_squared_error(y_true=df["y_true"], y_pred=df["y_pred"]))
-    result = {
-        "p": p,
-        "q": q,
-        "rmse": rmse,
-        "mean_aic": np.mean(aics),
-        "mean_bic": np.mean(bics),
-        "n_convergence_errors": n_convergence_errors,
-        "n_stationarity_errors": n_stationarity_errors,
-    }
-    return result
+        max_q : int, optional
+            The maximum number of lagged disturbances to test. Defaults to 5.
+
+        train_len : int, optional
+            Then length of the training set. Defaults to 120.
+
+        n_workers : int, optional
+            Number of cores to use for parallel evaluation of models.
+            Defaults to 6.
+
+        Returns
+        -------
+        pd.DataFrame
+            Returns a DataFrame, meant for analysis by other functions in this
+            module, that is the results of training with different p and q values.
+        """
+        tasks = []
+        for p in range(max_p):
+            for q in range(max_q):
+                if p == 0 and q == 0:
+                    continue
+                tasks.append(
+                    {
+                        "ts": ts,
+                        "d": d,
+                        "train_len": train_len,
+                        "p": p,
+                        "q": q,
+                    }
+                )
+        dask_client = Client(n_workers=n_workers)
+        futures = [dask_client.submit(p_d_q_fit, task) for task in tasks]
+        arima_fit_results = []
+        for future in tqdm(
+            as_completed(futures), total=len(futures), desc="Processing", unit="task"
+        ):
+            arima_fit_results.append(future.result())
+        dask_client.close()
+        arima_fit_df = pd.DataFrame(arima_fit_results)
+        arima_fit_df.set_index(["p", "q"], inplace=True)
+        return arima_fit_df
+
+
+    def p_d_q_fit(self, task):
+        """
+        Train and evaluate an ARIMA model with the given p, d, and q parameters
+        in the task dictionary.
+
+        Meant to be used by the train_arima function to perform parrallizable
+        tasks on cores.
+
+        Paremters
+        ---------
+        task : dict
+            Dictionary that defines the model training task from train_arima.
+
+        Returns
+        -------
+        dict
+            Returns a dictionary, suitable to be used as one row of the results
+            DataFrame returned by train_arima, that contains the results of the
+            training.
+        """
+        ts = task["ts"]
+        p = task["p"]
+        d = task["d"]
+        q = task["q"]
+        train_len = task["train_len"]
+        y_true = ts[train_len:]
+        y_pred = []
+        bics = []
+        aics = []
+        n_convergence_errors = 0
+        n_stationarity_errors = 0
+        for bound in range(train_len, len(ts)):
+            train_set = ts.iloc[bound - train_len : bound]
+            with warnings.catch_warnings():
+                warnings.filterwarnings("ignore")
+                try:
+                    model = tsa.ARIMA(endog=train_set, order=(p, d, q)).fit()
+                except LinAlgError:
+                    n_convergence_errors += 1
+                except ValueError:
+                    n_stationarity_errors += 1
+                forecast = model.forecast(steps=1)
+                y_pred.append(forecast.iloc[0])
+                aics.append(model.aic)
+                bics.append(model.bic)
+        df = (
+            pd.DataFrame({"y_true": y_true, "y_pred": y_pred})
+            .replace(np.inf, np.nan)
+            .dropna()
+        )
+        rmse = np.sqrt(mean_squared_error(y_true=df["y_true"], y_pred=df["y_pred"]))
+        result = {
+            "p": p,
+            "q": q,
+            "rmse": rmse,
+            "mean_aic": np.mean(aics),
+            "mean_bic": np.mean(bics),
+            "n_convergence_errors": n_convergence_errors,
+            "n_stationarity_errors": n_stationarity_errors,
+        }
+        return result
+    
+    def best_arima_model(self, ts, train_result):
+        """
+        Use training results from train_arima and make a final ARIMA
+        model that has the best p and q values, as determined by their
+        RMSE and BIC metrics.
+        """
+        best_p, best_q = train_result.rank().loc[:, ["rmse", "mean_bic"]].mean(1).idxmin()
+        print(best_p, best_q)
+        best_arima_model = tsa.ARIMA(endog=ts, order=(best_p, 0, best_q)).fit()
+        return best_arima_model
+    
+    def predict_1_step(self, orginal_ts, best_arima_model):
+        """
+        Predict the next time step beyond the training of the model.
+        Return the prediction in units of the original time series.
+
+        Parameters
+        ----------
+        original_ts : pd.Series
+            Original time series, neither transformed nor differenced
+
+        best_arima_model : ARIMA
+            The best trained ARIMA model as returned from best_arima_model.
+
+        Returns
+        -------
+        np.float64
+            The next value in the time series.
+        """
+        forecast = best_arima_model.forecast(steps=1)
+        pred = orginal_ts.iloc[-1] * np.exp(forecast.iloc[0])
+        return pred
 
 
 def train_arima_viz(train_result):
@@ -184,18 +225,6 @@ def train_arima_viz(train_result):
     axes[0].set_title("Root Mean Squared Error")
     axes[1].set_title("Mean Bayesian Information Criterion")
     fig.tight_layout()
-
-
-def best_arima_model(ts, train_result):
-    """
-    Use training results from train_arima and make a final ARIMA
-    model that has the best p and q values, as determined by their
-    RMSE and BIC metrics.
-    """
-    best_p, best_q = train_result.rank().loc[:, ["rmse", "mean_bic"]].mean(1).idxmin()
-    print(best_p, best_q)
-    best_arima_model = tsa.ARIMA(endog=ts, order=(best_p, 0, best_q)).fit()
-    return best_arima_model
 
 
 def plot_correlogram(ts0, nlags, title, residual_rolling=21, acf_plot_ymax=0.1):
@@ -242,26 +271,3 @@ def plot_correlogram(ts0, nlags, title, residual_rolling=21, acf_plot_ymax=0.1):
     axs[1][1].set_ylim(-acf_plot_ymax, acf_plot_ymax)
     fig.suptitle(f"{title}")
     fig.tight_layout()
-
-
-def predict_1_step(orginal_ts, best_arima_model):
-    """
-    Predict the next time step beyond the training of the model.
-    Return the prediction in units of the original time series.
-
-    Parameters
-    ----------
-    original_ts : pd.Series
-        Original time series, neither transformed nor differenced
-
-    best_arima_model : ARIMA
-        The best trained ARIMA model as returned from best_arima_model.
-
-    Returns
-    -------
-    np.float64
-        The next value in the time series.
-    """
-    forecast = best_arima_model.forecast(steps=1)
-    pred = orginal_ts.iloc[-1] * np.exp(forecast.iloc[0])
-    return pred
