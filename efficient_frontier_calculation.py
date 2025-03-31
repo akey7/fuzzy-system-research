11a6a97fc4794c9b6ae4b98bc2e0b116757a8510
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-03-30 18:55:35 -0600

    Issue #46
    Beginnings of efficient frontier modeling class.

---
+++ efficient_frontier_calculation.py
@@ -0,0 +1,13 @@
+import os
+import json
+import datetime
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize
+import pandas_datareader as web
+from dateutil.relativedelta import relativedelta
+
+
+class EfficientFrontierCalculation:
+    pass
